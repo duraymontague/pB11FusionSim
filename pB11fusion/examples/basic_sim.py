7345c0a821a3ffc266fa65e4cc9daa9ffb454b8b
duraymontague <duray.montague@gmail.com>
2025-12-27 01:15:32 -0500

    Add basic simulation for loss fraction calculation

---
+++ pB11fusion/examples/basic_sim.py
@@ -0,0 +1,7 @@
+from pB11fusion.core import loss_fraction
+
+n_e = 1e20  # m^-3
+n_i = 1e20  # m^-3
+T_e_ev = 1e5  # 100 keV example
+
+print(f"Loss fraction at {T_e_ev/1000} keV: {loss_fraction(n_e, n_i, T_e_ev):.3f}")
