f4960d2b7e6e1d134122279c5a5a29ea1dba9709
duraymontague <duray.montague@gmail.com>
2025-12-27 01:12:15 -0500

    Add setup.py for pB11FusionSim package

---
+++ pB11fusion/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pB11FusionSim',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=['numpy'],
+    description='Simple p-B11 fusion simulation package',
+    author='[Your Name]',
+    license='MIT',
+)
