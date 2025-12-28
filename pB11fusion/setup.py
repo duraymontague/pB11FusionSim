0c1b731c4e6c0d8f93f35a0906ca93fef20f482c
duraymontague <duray.montague@gmail.com>
2025-12-28 03:22:59 -0500

    Update setup.py with package metadata and requirements

--- pB11fusion/setup.py
+++ pB11fusion/setup.py
@@ -3,10 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name='pB11FusionSim',
     version='0.1.0',
+    description='A simple Python package for modeling p-B11 aneutronic fusion physics',
+    author='DuRay Montague',
+    author_email='duray@type2realist.com',
+    url='https://github.com/duraymontague/pB11FusionSim',
     packages=find_packages(),
-    install_requires=['numpy'],
-    description='Simple p-B11 fusion simulation package',
-    author='[Your Name]',
-    license='MIT',
+    install_requires=[
+        'numpy',  # add any dependencies here, e.g. numpy if your code uses it
+    ],
+    python_requires='>=3.6',
 )
- 
